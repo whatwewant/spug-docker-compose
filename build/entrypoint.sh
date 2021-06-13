c4d097356d351ef7cf0972149b58126a1f0f55a8
whatwewant <tobewhatwewant@outlook.com>
2021-06-14 01:47:15 +0800

    chore: fire

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-# Copyright: (c) OpenSpug Organization. https://github.com/openspug/spug
-# Copyright: (c) <spug.dev@gmail.com>
-# Released under the AGPL-3.0 License.
-
 set -e
 
 # init nginx
